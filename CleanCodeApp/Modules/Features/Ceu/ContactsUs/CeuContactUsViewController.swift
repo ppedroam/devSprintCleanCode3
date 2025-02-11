90111633a78f81f68290b3f7da0dd6e825cf6330
Jorge Roberto <jorgercj@dcomp.ufs.br>
2025-02-10 23:49:18 -0300

    Refactoring clicked functions

--- CleanCodeApp/Modules/Features/Ceu/ContactsUs/CeuContactUsViewController.swift
+++ CleanCodeApp/Modules/Features/Ceu/ContactsUs/CeuContactUsViewController.swift
@@ -70,7 +70,7 @@ class CeuContactUsViewController: LoadingInheritageController {
         sendMessageButton.setTitleColor(.white, for: .normal)
         sendMessageButton.layer.cornerRadius = 10
         sendMessageButton.setContentHuggingPriority(.required, for: .horizontal)
-        sendMessageButton.addTarget(self, action: #selector(messageSend), for: .touchUpInside)
+        sendMessageButton.addTarget(self, action: #selector(sendMessageButtonAction), for: .touchUpInside)
         return sendMessageButton
     }()
 
@@ -88,10 +88,13 @@ class CeuContactUsViewController: LoadingInheritageController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemGray6
+        setupViews()
 
-        // Criar botões
+        fetchContactUsData()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .systemGray6
         setupTitleLabel()
         setupPhoneButton()
         setupEmailButton()
@@ -100,8 +103,6 @@ class CeuContactUsViewController: LoadingInheritageController {
         setupCloseButton()
         setupSendMessageButton()
         setupTextView()
-        
-        pegarDados()
     }
 
     private func setupSubview(_ view: UIView, constraints: [NSLayoutConstraint]) {
@@ -180,13 +181,6 @@ class CeuContactUsViewController: LoadingInheritageController {
         ])
     }
 
-    private func handleClick(string: String?, urlString: String) {
-        if let string = string,
-           let url = URL(string: urlString + string) {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        }
-    }
-
     @objc
     func phoneClick() {
         let tel = model?.phone
@@ -198,72 +192,132 @@ class CeuContactUsViewController: LoadingInheritageController {
         let mail = model?.mail
         handleClick(string: mail, urlString: "mailto:")
     }
-    
-    @objc
-    func chatClicked() {
-        if let phoneNumber = model?.phone, let whatsappURL = URL(string: "whatsapp://send?phone=\(phoneNumber)&text=Oi)") {
-            if UIApplication.shared.canOpenURL(whatsappURL) {
-                UIApplication.shared.open(whatsappURL, options: [:], completionHandler: nil)
-            } else {
-                if let appStoreURL = URL(string: "https://apps.apple.com/app/whatsapp-messenger/id310633997") {
-                    UIApplication.shared.open(appStoreURL, options: [:], completionHandler: nil)
-                }
-            }
+
+    private func handleClick(string: String?, urlString: String) {
+        if let string = string,
+           let url = URL(string: urlString + string) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
-    
+
+    @objc
+    func chatClicked() throws {
+        let urls = try setupURLs()
+        let canOpenWhatsappURL = UIApplication.shared.canOpenURL(urls.whatsappURL)
+        if canOpenWhatsappURL {
+            openURL(url: urls.whatsappURL)
+        } else {
+            openURL(url: urls.appleStoreURL)
+        }
+
+    }
+
+    private func setupURLs() throws -> CeuUrlTypes {
+        let phoneNumber = model?.phone
+        guard let phoneNumber = phoneNumber else {
+            throw CeuContactUsCommonsErrors.invalidPhoneNumber
+        }
+        guard let whatsappURL = URL(string: "whatsapp://send?phone=\(phoneNumber)&text=Oi)") else {
+            throw CeuContactUsCommonsErrors.invalidURL
+        }
+        guard let appleStoreURL = URL(string: "https://apps.apple.com/app/whatsapp-messenger/id310633997") else {
+            throw CeuContactUsCommonsErrors.invalidURL
+        }
+        return CeuUrlTypes(appleStoreURL: appleStoreURL, whatsappURL: whatsappURL)
+    }
+
+    private func openURL(url: URL?) {
+        if let url = url {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+
     @objc
     func close() {
         dismiss(animated: true)
     }
-    
-    
-    func pegarDados() {
+
+    private func fetchContactUsData() {
         showLoadingView()
         let url = Endpoints.contactUs
         AF.shared.request(url, method: .get, parameters: nil, headers: nil) { result in
             self.removeLoadingView()
             switch result {
             case .success(let data):
-                let decoder = JSONDecoder()
-                if let returned = try? decoder.decode(ContactUsModel.self, from: data) {
-                    self.model = returned
-                } else {
-                    Globals.alertMessage(title: "Ops..", message: "Ocorreu algum erro", targetVC: self) {
-                        self.dismiss(animated: true)
-                    }
-                }
+                self.handleFetchContactUsDataResquestSuccess(data)
             case .failure(let error):
-                print("error api: \(error.localizedDescription)")
-                Globals.alertMessage(title: "Ops..", message: "Ocorreu algum erro", targetVC: self) {
-                    self.dismiss(animated: true)
-                }
+                self.handleFetchContactUsDataResquestError(error)
             }
         }
     }
-    
+
+    private func handleFetchContactUsDataResquestSuccess(_ data: Data) {
+        let decoder = JSONDecoder()
+        if let returned = try? decoder.decode(ContactUsModel.self, from: data) {
+            self.model = returned
+        } else {
+            self.showErrorAlertMessage()
+        }
+    }
+
+    private func handleFetchContactUsDataResquestError(_ error: Error) {
+        print("error api: \(error.localizedDescription)")
+        showErrorAlertMessage()
+    }
+
+    private func showErrorAlertMessage() {
+        Globals.alertMessage(title: "Ops..", message: "Ocorreu algum erro", targetVC: self) {
+            self.dismiss(animated: true)
+        }
+    }
+
+    private func showSuccessAlertMessage() {
+        Globals.alertMessage(title: "Sucesso..", message: "Sua mensagem foi enviada", targetVC: self) {
+            self.dismiss(animated: true)
+        }
+    }
+
     @objc
-    func messageSend() {
+    func sendMessageButtonAction() {
         view.endEditing(true)
-        let email = model?.mail ?? ""
-        if let message = textView.text, textView.text.count > 0 {
-            let parameters: [String: String] = [
-                "email": email,
-                "mensagem": message
-            ]
-            showLoadingView()
-            let url = Endpoints.sendMessage
-            AF.shared.request(url, method: .post, parameters: parameters, headers: nil) { result in
-                self.removeLoadingView()
-                switch result {
-                case .success:
-                    Globals.alertMessage(title: "Sucesso..", message: "Sua mensagem foi enviada", targetVC: self) {
-                        self.dismiss(animated: true)
-                    }
-                case .failure:
-                    Globals.alertMessage(title: "Ops..", message: "Ocorreu algum erro", targetVC: self)
-                }
+        guard let message = textView.text, textView.text.count > 0 else {
+            return
+        }
+        showLoadingView()
+        sendMessageRequest(message: message)
+    }
+
+    func sendMessageRequest(message: String) {
+        let parameters = setupParameters(message: message)
+        let url = Endpoints.sendMessage
+        AF.shared.request(url, method: .post, parameters: parameters, headers: nil) { result in
+            self.removeLoadingView()
+            switch result {
+            case .success:
+                self.showSuccessAlertMessage()
+            case .failure:
+                self.showErrorAlertMessage()
             }
         }
     }
+
+    private func setupParameters(message: String) -> [String: String] {
+        let email = model?.mail ?? ""
+        let parameters: [String: String] = [
+            "email": email,
+            "mensagem": message
+        ]
+        return parameters
+    }
+}
+
+enum CeuContactUsCommonsErrors: Error {
+    case invalidData
+    case invalidURL
+    case invalidPhoneNumber
+}
+
+struct CeuUrlTypes {
+    let appleStoreURL: URL
+    let whatsappURL: URL
 }

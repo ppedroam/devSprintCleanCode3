bb50bb716b03667799540466d58e53b658c4d47e
Jorge Roberto <jorgercj@dcomp.ufs.br>
2025-02-22 14:26:53 -0300

    [WIP] Create CeuResetPasswordViewModelTests

---
+++ CleanCodeAppTests/CeuResetPasswordViewModelTests.swift
@@ -0,0 +1,17 @@
+//
+//  CeuResetPasswordViewModelTests.swift
+//  CleanCodeAppTests
+//
+//  Created by Jorge de Carvalho on 21/02/25.
+//
+
+import Testing
+
+struct CeuResetPasswordViewModelTests {
+
+    // TODO: Tentar usar esse framwork novo.
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
